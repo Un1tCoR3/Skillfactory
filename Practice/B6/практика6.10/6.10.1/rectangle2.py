d2d7f177ae8aeda558c81f1ffcb7b1f3b12a8bbe
Un1tCoR3 <Serjik16011996@gmail.com>
2023-02-03 16:26:02 +0300

    message

--- Practice/B6/практика6.10/6.10.1/rectangle2.py
+++
@@ -1,4 +0,0 @@
-from rectangle import Rectangle
-
-rectangle_1 = Rectangle(5,10,50,100)
-print(rectangle_1.__class__.__name__, rectangle_1)
--- Practice/B6/практика6.10/6.10.3 кошелек/baza.py
+++
@@ -1,4 +0,0 @@
-from wallet import Wallet
-
-account = Wallet("Иван Иванов", 50)
-print(*account.get_account_info())
